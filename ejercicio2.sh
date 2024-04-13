1498e2ccf65ce35784ca3b982eb1fe613d8030b0
Rodrigo M <rodrigo@Ubuntu.myguest.virtualbox.org>
2024-04-13 15:45:18 -0600

    Realizar las verificaciones necesarias

--- ejercicio2.sh
+++ ejercicio2.sh
@@ -9,4 +9,16 @@ process_name=$1
 
 command=$2
 
+while True; do 
+    if pgrep -x "$process_name" >/dev/null; then
+        echo "El proceso esta corriendo"
+    else
+        echo "El proceso no esta corriendo, se levantara"
+        $command &
+    fi 
+
+    slepp 10
+
+done 
+
 

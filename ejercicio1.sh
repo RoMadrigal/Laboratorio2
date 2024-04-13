3da8ca0e25c028616ba770a186017eb574145d1a
Rodrigo M <rodrigo@Ubuntu.myguest.virtualbox.org>
2024-04-13 15:16:57 -0600

    Obtener datos necesarios

--- ejercicio1.sh
+++ ejercicio1.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
 
+process_pid=$1
+
+process_name=$(ps -p "$process_pid" -o comm)
+
+process_id=$(ps -p "$process_pid" -o pid)
+
+process_parent=$(ps -p "$process_pid" -o ppid)
+
+process_user=$(ps -p "$process_pid" -o user)
+
+process_CPU=$(ps -p "$process_pid" -o %cpu) 
+
+process_memo=$(ps -p "$process_pid" -o %mem)
+
+process_status=$(ps -p "$process_pid" -o stat)
+
+process_path=$(readlink -f "/proc/$process_pid/exe")
+
 
 

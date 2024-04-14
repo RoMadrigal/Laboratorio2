b3a1df77db5042d1c0781aaa778ee660a3d9896f
Rodrigo M <rodrigo@Ubuntu.myguest.virtualbox.org>
2024-04-13 18:55:15 -0600

    Monitoreo continuo del directorio

--- ejercicio4.sh
+++ ejercicio4.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
+directorio="/home/rodrigo"
+
+inotifywait -m -r -e create,modify,delete "$directorio" |
+
+    while read -r directory event filename; do 
+
+        echo "$(date +%Y/%m/%d %H:%M:%S) - $event: $directoty/$filename" >> /home/rodrigo/Documents/registro.txt
+    done 
+
+
+
 

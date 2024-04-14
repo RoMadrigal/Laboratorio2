9edb8004b43b7bd0774da1180d682e47d67bfcf2
Rodrigo M <rodrigo@Ubuntu.myguest.virtualbox.org>
2024-04-13 18:19:16 -0600

    Obtiene los consumos y envia a registro

--- ejercicio3.sh
+++ ejercicio3.sh
@@ -6,4 +6,22 @@ registro="log_$ejecutable.txt"
 
 ./$ejecutable &
 
+exepid=$!
+
+echo "Hora ConsumoCPU ConsumoMemoria" > "$registro"
+
+while ps -p "$exepid" >/dev/null; do
+   
+    hora=$(date +%Y/%m/%d %H:%M:%S)
+ 
+    usoCPU=$(ps -p "$exepid" -o %cpu | tail -n 1)
+    
+    usoMemo=$(ps -p "$exepid" -o %mem | tail -n 1)
+
+    echo "$hora $usoCPU $usoMemo" >> "$registro"
+
+    sleep 5
+
+done 
+
 
